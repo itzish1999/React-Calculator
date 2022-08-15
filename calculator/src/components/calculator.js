9bb670732f1cc26590522689ebdfea87b0b9d0b6
itzish1999 <ismaeel.butt19@gmail.com>
2022-08-14 20:42:30 -0400

    feat(Components Setup)

--- calculator/src/components/calculator.js
+++ calculator/src/components/calculator.js
@@ -1,12 +1,47 @@
 //Parent Component
-
 import React from 'react';
+import CalculatorTitle from './calculatorTitle';
+import OutputScreen from './outputScreen';
+import Button from './button';
 
 class Calculator extends React.Component {
     render()
     {
-        return "Calulator";
+        <div className="frame">
+            <CalculatorTitle value="Calculator" />
+            <div class="mainCalc">
+                <OutputScreen />
+                <div className="button-row">
+                    <Button label={"Clear"} />
+                    <Button label={"Delete"} />
+                    <Button label={"."} />
+                    <Button label={"/"} />
+                </div>
+                <div className="button-row">
+                    <Button label={"7"} />
+                    <Button label={"8"} />
+                    <Button label={"9"} />
+                    <Button label={"*"} />
+                </div>
+                <div className="button-row">
+                    <Button label={"4"} />
+                    <Button label={"5"} />
+                    <Button label={"6"} />
+                    <Button label={"-"} />
+                </div>
+                <div className="button-row">
+                    <Button label={"1"} />
+                    <Button label={"2"} />
+                    <Button label={"3"} />
+                    <Button label={"+"} />
+                </div>
+                <div classname="button-row">
+                    <Button label={"0"} />
+                    <Button label={"="} />
+                </div>
+            </div>
+        </div>
     }
-}
+};
 
 export default Calculator;
