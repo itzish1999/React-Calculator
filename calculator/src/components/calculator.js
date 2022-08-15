00164345f26c759fddc3cc18fd21cb23c9a6ae52
itzish1999 <ismaeel.butt19@gmail.com>
2022-08-14 20:01:39 -0400

    feat(backbone)

---
+++ calculator/src/components/calculator.js
@@ -0,0 +1,12 @@
+//Parent Component
+
+import React from 'react';
+
+class Calculator extends React.Component {
+    render()
+    {
+        return "Calulator";
+    }
+}
+
+export default Calculator;
